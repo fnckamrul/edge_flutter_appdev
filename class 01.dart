7cb79bf133ab82866b99c6d2a76abfe99ac011f8
fnckamrul <fnckamrul@gmail.com>
2024-11-16 23:05:58 +0600

    concetination string

--- class 01.dart
+++ class 01.dart
@@ -24,31 +24,9 @@ void main() {
 
 
 
-  ///================String==================
-  ///singale line string
-  // String singleLine = "This is single line";
-  // print(singleLine);
-  ///multiple line string
-  // String multiLine = """This is Multiple line string""";
-  // print(multiLine);
-
-  ///=============== Concetination String====================
-  //   String multiLine = """ This is Multiple line string """;
-  //   String singleLine = "This is single line";
-  //   String splitLine = "Home, About, Product, Service";
-  // String con = singleLine+multiLine;
-  // print(con);
-  // print("This is single: ${singleLine} and This multiline: ${multiLine}");
 
 
-  ///property of string
-  // print(singleLine.length);
-  ///methods  of string
-  // print(singleLine.toUpperCase());
-  // print(singleLine.trim());
-  // print(singleLine.toLowerCase());
-  // print(splitLine.split(','));
-  // print(singleLine.substring(0,7));
+
 
   ///=====================List Using Dart==================///
   // var cars = ['Model1', 'Model2', 'Model3'];
