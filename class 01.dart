37cd247766049f49c093b9dd4db7655ec5792346
fnckamrul <fnckamrul@gmail.com>
2024-11-16 10:07:19 +0600

    first commit

---
+++ class 01.dart
@@ -0,0 +1,187 @@
+
+///Date: 12/11/24
+void main() {
+  ///student info:
+  var name = "Jhon";
+  var age = 20;
+  var num = 10;
+  var gender = 'Male';
+  print("Our new student name is: ${name}.${name} is ${age} years old.${age}");
+
+
+// print(5*6);
+// print(num>age);
+//   var num1=5;//101
+//   var num2 =3;//011
+//   var num3 =  num1 | num2;
+//   var num4 = 10;
+//   num4 += 21;
+//   print(num3);
+//   print(num4);
+//
+
+  ///For Loop and nested loop
+//   for(var i=0; i<=10; i++){
+//     print("This is Line: ${i}");
+//     for(var j = 1; j<=5; j++){
+//       print("${i}.${j}: Subline: ${j}");
+//     }
+//   }
+//   print("Loop end here");
+
+  ///While Loop
+// var i = 1;
+// while(i<=5){
+//   print ("the number is ${i}");
+//   i++;
+// }
+// print("Loop End Here");
+//
+
+  ///Do While loop
+//   var n = 6;
+//   do{
+//     print("The number is : ${n}");
+//     n++;
+//   }while(n<=5);
+//   print("Loop end here");
+//
+
+// var num = 6;
+// if(num>5){
+//   print("${num} Number is grater than 5");
+// }
+//
+// else{
+//   print("${num} is less than 5");
+// }
+//
+//
+
+  ///switch case statement
+//   var day = 3;
+//
+//   switch(day){
+//     case 0:
+//       print("Sunday");
+//     case 1:
+//       print("monday");
+//     case 2:
+//       print("Tue");
+//     case 3:
+//       print("Wed");
+//     case 4:
+//       print("Thu");
+//     case 5:
+//       print("Fri");
+//
+//   }
+
+
+
+
+
+  /// Date 14/11/2024
+
+  ///====================Numbers=========================
+  ///======Parsing====
+// int num1 = 10;
+// double num2 = 10.91;
+// print (num.parse('10'));
+// print(num.parse('10.91'));
+
+  ///=================Number properties=================
+  ///hashCode
+//   var n = 20;
+//   print(n.hashCode);
+    ///isFinite
+//   var x = 50;
+//   print(x.isFinite);
+  /// isNeagative
+//   var z = -200;
+//   print(z.isNegative);
+  ///	isEven
+//   var a = 50;
+//   print(a.isEven);
+//   //isOdd
+//   var b = 5;
+//   print(b.isOdd);
+//
+  ///Absolute method
+//   void main(){
+//     int num1 = -23;
+//     print(num1.abs());
+  //}
+  ///truncate
+// double num = 23.523456;
+// print(num.truncate());
+
+
+
+
+  ///================String==================
+  ///singale line string
+  // String singleLine = "This is single line";
+  // print(singleLine);
+  ///multiple line string
+  // String multiLine = """This is Multiple line string""";
+  // print(multiLine);
+
+  ///=============== Concetination String====================
+  //   String multiLine = """ This is Multiple line string """;
+  //   String singleLine = "This is single line";
+  //   String splitLine = "Home, About, Product, Service";
+  // String con = singleLine+multiLine;
+  // print(con);
+  // print("This is single: ${singleLine} and This multiline: ${multiLine}");
+
+
+  ///property of string
+  // print(singleLine.length);
+  ///methods  of string
+  // print(singleLine.toUpperCase());
+  // print(singleLine.trim());
+  // print(singleLine.toLowerCase());
+  // print(splitLine.split(','));
+  // print(singleLine.substring(0,7));
+
+  ///=====================List Using Dart==================///
+  // var cars = ['Model1', 'Model2', 'Model3'];
+  // // print("This is Model Number : ${cars[2]}");
+  // print()
+
+
+
+
+  // var fruits = new List(3);
+  // fruits[0] = "Apple";
+  // fruits[1] = "Banana";
+  // fruits[2] = "Apple";
+
+
+  ///=============Map===============================
+  // var studentInfo = {
+  //     'name': 'John',
+  //     'id': 0125654,
+  //     'age': 20,
+  //     'Gender': 'Male'
+  // };
+  // print(studentInfo);
+
+  // var studentInfo = new Map();
+  // studentInfo['name'] = 'Jhone';
+  // studentInfo['id']  = 123;
+  // print(studentInfo);
+
+  ///for Each(), symbole , branch(next class learning)
+
+  ///==================Functions=====================
+//   message();///calling function
+
+
+// }
+
+
+// message(){
+//   print("We are learning dart and calling function");
+// }
