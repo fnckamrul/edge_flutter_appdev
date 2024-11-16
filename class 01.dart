ebf1be0ae357dfbf5e9e5b03df925360702119ed
fnckamrul <fnckamrul@gmail.com>
2024-11-16 23:02:42 +0600

    string

--- class 01.dart
+++ class 01.dart
@@ -20,34 +20,6 @@ void main() {
 
 
 
-  /// Date 14/11/2024
-
-
-  ///=================Number properties=================
-  ///hashCode
-//   var n = 20;
-//   print(n.hashCode);
-    ///isFinite
-//   var x = 50;
-//   print(x.isFinite);
-  /// isNeagative
-//   var z = -200;
-//   print(z.isNegative);
-  ///	isEven
-//   var a = 50;
-//   print(a.isEven);
-//   //isOdd
-//   var b = 5;
-//   print(b.isOdd);
-//
-  ///Absolute method
-//   void main(){
-//     int num1 = -23;
-//     print(num1.abs());
-  //}
-  ///truncate
-// double num = 23.523456;
-// print(num.truncate());
 
 
 
---
+++ string.dart
@@ -0,0 +1,12 @@
+void main(){
+    ///================String===============
+    ///singale line string
+
+  String singleLine = "This is single line";
+  print(singleLine);
+  
+  //multiple line string
+  String multiLine = """This is Multiple line string""";
+  print(multiLine);
+
+}
