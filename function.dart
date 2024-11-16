91f798872d84e07121a695cb3aa82d219a5c3259
fnckamrul <fnckamrul@gmail.com>
2024-11-17 00:12:37 +0600

    function

---
+++ function.dart
@@ -0,0 +1,11 @@
+ void main(){
+  
+
+  ///==================Functions=====================
+  message();///calling function
+
+}
+message(){
+  print("We are learning dart and calling function");
+}
+
