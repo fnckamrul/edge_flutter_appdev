5bf5f58502304e79fe391e767dbc58f20ce617d9
fnckamrul <fnckamrul@gmail.com>
2024-11-17 00:05:15 +0600

    Lit

--- class 01.dart
+++ class 01.dart
@@ -8,49 +8,12 @@ void main() {
   var gender = 'Male';
   print("Our new student name is: ${name}.${name} is ${age} years old.${age}");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  ///=============Map===============================
-  // var studentInfo = {
-  //     'name': 'John',
-  //     'id': 0125654,
-  //     'age': 20,
-  //     'Gender': 'Male'
-  // };
-  // print(studentInfo);
-
-  // var studentInfo = new Map();
-  // studentInfo['name'] = 'Jhone';
-  // studentInfo['id']  = 123;
-  // print(studentInfo);
-
-  ///for Each(), symbole , branch(next class learning)
-
-  ///==================Functions=====================
-//   message();///calling function
-
-
-// }
-
-
-// message(){
-//   print("We are learning dart and calling function");
-// }
+///=========Map======
+  var studentInfo = {
+      'name': 'John',
+      'id': 0125654,
+      'age': 20,
+      'Gender': 'Male'
+  };
+  print(studentInfo);
+}
---
+++ map.dart
@@ -0,0 +1,12 @@
+  ///=============Map===============================
+  void main(){
+  var studentInfo = new Map();
+  studentInfo['name'] = 'Jhone';
+  studentInfo['ID']  = 123;
+  studentInfo['number']  = 014041;
+  studentInfo['Gender']  = 'Male';
+  print(studentInfo);
+
+  //for Each(), symbole , branch(next class learning)
+
+  }
