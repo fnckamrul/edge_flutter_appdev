c4ac316997ca25ed7c6db0f8127da0b437448bf2
fnckamrul <fnckamrul@gmail.com>
2024-12-03 19:03:38 +0600

    abstract

--- abstract.dart
+++ abstract.dart
@@ -1,24 +1,24 @@
-// void main(){
-//   var son = Son();
-//   son.methodOne();
+void main(){
+  var son = Son();
+  son.methodOne();
   
-// }
-// abstract class Father{
-//   int age = 52;
-//   methodOne(){
-//     print("i am in true");
-//   }
-//   methodTwo(){
-//     print("i am in false");
-//   }
+}
+abstract class Father{
+  int age = 52;
+  methodOne(){
+    print("i am in true");
+  }
+  methodTwo(){
+    print("i am in false");
+  }
 
-// }
+}
 
-// class Son extends Father{
-//    @override 
-//    methodOne() {
-//     print("hello abstract method in dart");
+class Son extends Father{
+   @override 
+   methodOne() {
+    print("hello abstract method in dart");
     
-//   }
-// }
+  }
+}
 
